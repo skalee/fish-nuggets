1ec5c7d453353cd1bbfae9755d52d2aad96642a1
Brian Donovan <brian.donovan@gmail.com>
2009-02-09 10:15:16 -0800

    Use blue for no changes, red for changes. Bold just isn't cutting it.

--- functions/parse_git_branch.fish
+++ functions/parse_git_branch.fish
@@ -1,7 +1,9 @@
 function parse_git_branch
   if is-git
     if test ! (git status | grep 'working directory clean')
-      set_color --bold
+      set_color red
+    else
+      set_color blue
     end
     git branch --no-color ^/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \/\1/'
   end
