68bba9409ff73798cc5f74078bca055d11e9f389
Brian Donovan <brian.donovan@gmail.com>
2010-03-31 15:21:24 -0700

    Remove the HEYSPLITHERE hack and sync rvm output.

--- functions/rvm.fish
+++ functions/rvm.fish
@@ -1,23 +1,22 @@
 function rvm
+  # save original environment
   set -l fish_env (env)
-  set -l bash_env (bash -c "source ~/.rvm/scripts/rvm; rvm $argv; echo HEYSPLITHERE; env")
 
+  # run rvm and capture the resulting environment
+  set -l env_file (mktemp -t rvm)
+  bash -e -c "source ~/.rvm/scripts/rvm; rvm $argv; env > $env_file"
+  set -l bash_env (cat $env_file)
+  rm -f $env_file
+
+  # get the env values from rvm that were present in fish
   set -l common
 
   for env in $bash_env
-    if test -z "$capture"
-      if test $env = 'HEYSPLITHERE'
-        set capture '1'
-      else
-        echo $env
-      end
-      continue
-    end
-
     if not contains $env $fish_env
       set common $common $env
     end
   end
 
+  # write the env values to the current fish session
   __bash_env_to_fish $common
 end
