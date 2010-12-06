12dc2c985c5e26cc2efcceb2b7bbf4dc457afef0
Brian Donovan <me@brian-donovan.com>
2010-12-06 10:39:40 -0800

    The cost of looking up hg branch info just isn't worth the small number of repos I have.

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description "Write out the prompt"
-  printf '%s%s %s%s%s%s%s $' (set_color cyan) (whoami) (set_color yellow) (prompt_pwd) (parse_git_branch) (parse_hg_branch) (set_color yellow)
+  printf '%s%s %s%s%s%s%s $' (set_color cyan) (whoami) (set_color yellow) (prompt_pwd) (parse_git_branch) (set_color yellow)
   set_color normal
   printf ' '
 end
