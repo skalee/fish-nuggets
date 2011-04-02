31f447b27e4c639b6e2bac55698f0dca3aeb1278
Sebastian Skalacki <sebastian.skalacki@gmail.com>
2011-04-02 13:57:13 +0200

    Skalee's prompt.

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,5 +1,3 @@
 function fish_prompt --description "Write out the prompt"
-  printf '%s%s %s%s%s%s%s $' (set_color cyan) (whoami) (set_color yellow) (prompt_pwd) (parse_git_branch) (set_color yellow)
-  set_color normal
-  printf ' '
+	printf '%s%s%s %s %s%s%s%s>' (set_color cyan) (date +%H:%M) (set_color normal) (whoami) (set_color $fish_color_cwd) (prompt_pwd) (parse_git_branch) (set_color normal)
 end
